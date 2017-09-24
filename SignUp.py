7c2a949c67b6467ebb147fa67b14974a922d181f
Ahmed <ahmed.l0m3y@gmail.com>
2017-09-24 22:17:08 +0200

    working File without error

--- SignUp.py
+++ SignUp.py
@@ -2,84 +2,70 @@
 
 # Form implementation generated from reading ui file 'SignUp.ui'
 #
-# Created by: PyQt4 UI code generator 4.11.4
+# Created by: PyQt5 UI code generator 5.9
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Form(object):
     def setupUi(self, Form):
-        Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(500, 360)
-        Form.setStyleSheet(_fromUtf8("background-image: url(:/img/Screenshot from 2017-09-20 17-37-17.png);"))
-        self.gridLayout = QtGui.QGridLayout(Form)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.lineEdit_4 = QtGui.QLineEdit(Form)
-        self.lineEdit_4.setObjectName(_fromUtf8("lineEdit_4"))
+        Form.setObjectName("Form")
+        Form.resize(600, 360)
+        Form.setStyleSheet("background-image: url(:/new/prefix1/2017.jpg);")
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lineEdit_4 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_4.setObjectName("lineEdit_4")
         self.gridLayout.addWidget(self.lineEdit_4, 1, 2, 1, 1)
-        self.pushButton = QtGui.QPushButton(Form)
-        self.pushButton.setStyleSheet(_fromUtf8("color: rgb(255, 255, 255);\n"
-"font: 63 oblique 12pt \"URW Gothic L\";"))
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setStyleSheet("color: rgb(255,250 , 255);")
+        self.pushButton.setObjectName("pushButton")
         self.gridLayout.addWidget(self.pushButton, 6, 2, 1, 1)
-        self.lineEdit_6 = QtGui.QLineEdit(Form)
-        self.lineEdit_6.setInputMask(_fromUtf8(""))
-        self.lineEdit_6.setPlaceholderText(_fromUtf8(""))
-        self.lineEdit_6.setObjectName(_fromUtf8("lineEdit_6"))
+        self.lineEdit_6 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_6.setInputMask("")
+        self.lineEdit_6.setPlaceholderText("")
+        self.lineEdit_6.setObjectName("lineEdit_6")
         self.gridLayout.addWidget(self.lineEdit_6, 3, 2, 1, 1)
-        self.lineEdit = QtGui.QLineEdit(Form)
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setObjectName("lineEdit")
         self.gridLayout.addWidget(self.lineEdit, 1, 0, 1, 1)
-        self.lineEdit_2 = QtGui.QLineEdit(Form)
-        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.lineEdit_2 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_2.setObjectName("lineEdit_2")
         self.gridLayout.addWidget(self.lineEdit_2, 2, 0, 1, 1)
-        self.lineEdit_5 = QtGui.QLineEdit(Form)
-        self.lineEdit_5.setObjectName(_fromUtf8("lineEdit_5"))
+        self.lineEdit_5 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_5.setObjectName("lineEdit_5")
         self.gridLayout.addWidget(self.lineEdit_5, 2, 2, 1, 1)
-        self.lineEdit_3 = QtGui.QLineEdit(Form)
-        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
+        self.lineEdit_3 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_3.setObjectName("lineEdit_3")
         self.gridLayout.addWidget(self.lineEdit_3, 3, 0, 1, 1)
-        self.pushButton_2 = QtGui.QPushButton(Form)
-        self.pushButton_2.setStyleSheet(_fromUtf8("color: rgb(255, 255, 255);\n"
-"font: 63 oblique 12pt \"URW Gothic L\";"))
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setStyleSheet("color: rgb(255, 255, 255);")
+        self.pushButton_2.setObjectName("pushButton_2")
         self.gridLayout.addWidget(self.pushButton_2, 6, 0, 1, 1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(_translate("Form", "Form", None))
-        self.lineEdit_4.setText(_translate("Form", "Car Number", None))
-        self.pushButton.setText(_translate("Form", "Save", None))
-        self.lineEdit_6.setText(_translate("Form", "Car Color", None))
-        self.lineEdit.setText(_translate("Form", "Your Name", None))
-        self.lineEdit_2.setText(_translate("Form", "Passward", None))
-        self.lineEdit_5.setText(_translate("Form", "Car model", None))
-        self.lineEdit_3.setText(_translate("Form", "Comf Password", None))
-        self.pushButton_2.setText(_translate("Form", "Cancel", None))
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.lineEdit_4.setText(_translate("Form", "Car Number"))
+        self.pushButton.setText(_translate("Form", "Save"))
+        self.lineEdit_6.setText(_translate("Form", "Car Color"))
+        self.lineEdit.setText(_translate("Form", "Your Name"))
+        self.lineEdit_2.setText(_translate("Form", "Passward"))
+        self.lineEdit_5.setText(_translate("Form", "Car model"))
+        self.lineEdit_3.setText(_translate("Form", "Comf Password"))
+        self.pushButton_2.setText(_translate("Form", "Cancel"))
 
-import iamge_rc
+
+import image_rc
 
 if __name__ == "__main__":
     import sys
-    app = QtGui.QApplication(sys.argv)
-    Form = QtGui.QWidget()
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
     ui = Ui_Form()
     ui.setupUi(Form)
     Form.show()
--- main.py
+++ main.py
@@ -1,35 +1,59 @@
 import sys
-from PyQt4.QtGui import QWidget, QApplication
+from PyQt5.QtCore import QSize
+from PyQt5 import QtCore, QtGui, QtWidgets as Q
+from PyQt5.QtGui import QImage, QPalette, QBrush
 
+import image_rc
 from SignIN import Ui_Form as SignInForm
 from WelFrame import Ui_Form as WelFrameForm
 from SignUp import Ui_Form as SignUpForm
 from Accounting import Ui_Form as AccountForm
 
 
-class SignIn(QWidget, SignInForm):
+class SignIn(Q.QWidget, SignInForm):  # Widget
     def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
+        super(SignIn, self).__init__(parent)
+        Q.QWidget.__init__(self, parent)
         self.setupUi(self)
+        oImage = QImage("2017.jpg")
+        sImage = oImage.scaled(QSize(600, 360))  # resize Image to widgets size
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))  # 10 = WindowRole
+        self.setPalette(palette)
 
 
-class WelFrame(QWidget, WelFrameForm):
+class WelFrame(Q.QWidget, WelFrameForm):  # MainWindow
     def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
+        Q.QWidget.__init__(self, parent)
         self.setupUi(self)
+        oImage = QImage("2017.jpg")
+        sImage = oImage.scaled(QSize(600, 360))  # resize Image to widgets size
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))  # 10 = WindowRole
+        self.setPalette(palette)
         self.move(300, 200)
 
 
-class SignUp(QWidget, SignUpForm):
+class SignUp(Q.QWidget, SignUpForm):  # Widget
     def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
+        Q.QWidget.__init__(self, parent)
         self.setupUi(self)
+        oImage = QImage("2017.jpg")
+        sImage = oImage.scaled(QSize(600, 360))  # resize Image to widgets size
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))  # 10 = WindowRole
+        self.setPalette(palette)
 
 
-class Accout(QWidget, AccountForm):
+class Accout(Q.QWidget, AccountForm):  # Widget
     def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
+        Q.QWidget.__init__(self, parent)
         self.setupUi(self)
+        oImage = QImage("2017.jpg")
+        sImage = oImage.scaled(QSize(600, 360))  # resize Image to widgets size
+        palette = QPalette()
+        palette.setBrush(10, QBrush(sImage))  # 10 = WindowRole
+        self.setPalette(palette)
 
 
 def foo(w1, w2):
@@ -39,7 +63,7 @@ def foo(w1, w2):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = Q.QApplication(sys.argv)
     wel = WelFrame()
     signIn = SignIn()
     signUp = SignUp()
